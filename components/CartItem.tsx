fd93bbb6b142572f3d9a27842aa692f470c238d0
Ican09 <kaisanfarasdag09@gmail.com>
2026-01-04 21:10:44 +0800

    feat(cart): fix cart page layout

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useCartStore } from "@/store/cart";
+import { Trash2 } from "lucide-react";
 
 type Props = {
   id: string;
@@ -37,32 +38,37 @@ export default function CartItem({
         />
       )}
 
-      <div className="flex-1">
+      <div className="flex-1 flex flex-col justify-center">
         <h2 className="text-body-medium text-dark-900">{name}</h2>
-        <p className="text-body text-dark-700">${price.toFixed(2)}</p>
+        <p className="text-dark-700">{category}</p>
+        <div className="flex items-center gap-2 text-sm">
+          <p className="text-gray-400">Size <span className="text-black font-semibold">{size}</span></p>
+          <div className="flex items-center gap-3 bg-gray-200 rounded-full p-1">
+            <p className="text-gray-400">Quantity</p>
+            <button onClick={() => updateQuantity(id, quantity - 1)}>−</button>
 
-        <div className="mt-3 flex items-center gap-3">
-          <button onClick={() => updateQuantity(id, quantity - 1)}>−</button>
+            <span>{quantity}</span>
+            <button onClick={() => updateQuantity(id, quantity + 1)}>+</button>
 
-          <span>{quantity}</span>
-          <button onClick={() => updateQuantity(id, quantity + 1)}>+</button>
-
-          <button
-            onClick={() => removeItem(id)}
-            className="ml-4 text-red-600"
-          >
-            Remove
-          </button>
+            
+          </div>
         </div>
+
+        
+      </div>
+      <div className="flex justify-between flex-col">
+        <p className="text-body-medium text-dark-900">
+          ${(price * quantity).toFixed(2)}
+        </p>
+        <button
+          onClick={() => removeItem(id)}
+          className=" text-red-500 flex justify-end"
+        >
+          <Trash2 />
+        </button>
       </div>
 
-      <p className="text-body-medium text-dark-900">
-        ${(price * quantity).toFixed(2)}
-      </p>
-          <span>Size : {size}</span>
-          <span>Category : {category}</span>
-
-
+      
     </div>
   );
 }
