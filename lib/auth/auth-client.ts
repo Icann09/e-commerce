6954b524a27eaaec206f3142786f388f9e63ee0f
Ican09 <kaisanfarasdag09@gmail.com>
2026-01-06 20:36:36 +0800

    fix(auth): fix baseUrl

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://e-commerce-git-main-ican09s-projects.vercel.app"
+    baseURL: "https://e-commerce-two-ashen-45.vercel.app"
 })
-
